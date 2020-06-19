5f68073121e010831114120ee6e2169f0e866ac2
abhijitbonik <abhijitbonik@gmail.com>
2020-06-19 19:46:01 +0530

    menu and homme page  updated

--- frontend/src/app/menus/menus.component.ts
+++ frontend/src/app/menus/menus.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-menus',
@@ -8,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class MenusComponent implements OnInit {
   title = 'BirdNest Learning';
   constructor() { }
-
-
+  @ViewChild('navBurger') navBurger: ElementRef;
+  @ViewChild('navMenu') navMenu: ElementRef;
   ngOnInit(): void {
   }
+
+  toggleNavbar() { 
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
+ }
 
 }
